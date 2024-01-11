5cd932f8023736984c16e742d5d970fc3b0debef
Vinicus <viniciushacrinha1@gmail.com>
2024-01-11 14:54:27 -0300

    initial

---
+++ src/main/java/com/vinibank/vinibank/ViniBankApplication.java
@@ -0,0 +1,13 @@
+package com.vinibank.vinibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViniBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViniBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinibank/vinibank/ViniBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinibank.vinibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViniBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
